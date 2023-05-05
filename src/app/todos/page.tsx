24d3f606c72ff7d6e22755e362337d7f4dbfde17
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-05-05 17:37:23 +0500

    make new project add todos pages integrate jsonplaceholer todo api

---
+++ src/app/todos/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodosList from './TodosList'
+
+const Home = () => {
+  return (
+    <div>
+        {/* @ts-ignore */}
+        <TodosList/>
+    </div>
+  )
+}
+
+export default Home
