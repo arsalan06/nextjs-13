3bd76d594882e617fae0de782895000768d97c73
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-05-05 17:51:36 +0500

    add dynamic route for sigle todo

--- src/app/todos/[todoId]/page.tsx
+++ src/app/todos/[todoId]/page.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
-type PageProps={
-    params:{
-        todoId:string
-    }
-}
-function TodoPage({params:{todoId}}:PageProps) {
+import React from "react";
+import { Todo } from "../../../../typings";
+type PageProps = {
+  params: {
+    todoId: string;
+  };
+};
+const fetchTodos = async (todoId: string) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+  );
+  const todos: Todo[] = await res.json();
+  return todos;
+};
+async function TodoPage({ params: { todoId } }: PageProps) {
+  const todos = await fetchTodos(todoId);
   return (
-    <div>TodoPage:{todoId}</div>
-  )
+    <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
+      <p>
+        #{todos.id}:{todos.title}
+      </p>
+    </div>
+  );
 }
 
-export default TodoPage
+export default TodoPage;
